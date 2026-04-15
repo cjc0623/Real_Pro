7a09e4282c3b2a25b596eccc7b5e6b9d88dd57e6
gktmdwns1037 <gktmdwns1037@gmail.com>
2026-04-15 17:13:59 +0900

    로그인, 회원가입, 간편조회 수정

--- front/src/pages/SignUpPage.js
+++ front/src/pages/SignUpPage.js
@@ -2,12 +2,14 @@ import SignUpComponent from '../layout/component/users/SignUPComponent';
 
 const SignUpPage = () => {
     return (
-        <div className="fixed top-0 left-0 z-[1055] flex flex-col h-full w-full">
-            <div className="flex flex-wrap w-full h-full justify-center items-center border-2">
-                <SignUpComponent />
+        <div className="flex flex-col w-full justify-center items-center py-10 bg-white">
+            <div className="w-full max-w-[600px] px-4">
+                <div className="h-auto">
+                    <SignUpComponent />
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default SignUpPage;
